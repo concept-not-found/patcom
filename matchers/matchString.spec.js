65611d3a3ace97e5d54f195f00cc5e5fea432dce
Pyrolistical <pyrogx1133@gmail.com>
2022-02-26 22:47:51 -0800

    fixed bug where asMatcher was returning wrong matcher for boolean, number and string fixed bug for matchString where did not handle new Strings correctly for either the expected nor the value

---
+++ matchers/matchString.spec.js
@@ -0,0 +1,33 @@
+import { expectMatched } from './test-utils.js'
+
+import { matchString } from './index.js'
+
+describe('matchString', () => {
+  test('matched exact string', () => {
+    const matcher = matchString('Alice')
+    const result = matcher('Alice')
+    expectMatched(result)
+    expect(result.value).toBe('Alice')
+  })
+
+  test('matched when value is new String', () => {
+    const matcher = matchString('Alice')
+    const value = new String('Alice')
+    const result = matcher(value)
+    expectMatched(result)
+    expect(result.value).toBe(value)
+  })
+
+  test('matched expected is new String', () => {
+    const matcher = matchString(new String('Alice'))
+    const result = matcher('Alice')
+    expectMatched(result)
+    expect(result.value).toBe('Alice')
+  })
+
+  test('unmatched to different string', () => {
+    const matcher = matchString('Ailce')
+    const result = matcher('Bob')
+    expect(result.matched).toBe(false)
+  })
+})
